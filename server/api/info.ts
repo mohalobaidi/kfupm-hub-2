764414b41bfbd4af18d8587d0c5f87b988f0f63e
Mohammed Alobaidi <contact@mohalobaidi.com>
2024-05-04 09:44:14 +0300

    .

--- server/api/info.ts
+++ server/api/info.ts
@@ -1,11 +1,16 @@
-export default defineEventHandler(event => {
+export default defineEventHandler(async event => {
   const cityHeader = getHeader(event, 'x-vercel-ip-city')
   const city = cityHeader ? decodeURIComponent(cityHeader) : '-'
   const ipHeader = getHeader(event, 'x-forwarded-for')
   const ip = ipHeader ? ipHeader.split(',')[0] : '-'
+  const storage = useStorage('data')
+  let pageVisits = (await storage.getItem('pageVisits')) as number
+  const updatedPageVisits = pageVisits + 1
+  await storage.setItem('pageVisits', updatedPageVisits)
 
   return {
     city,
-    ip
+    ip,
+    pageVisits: updatedPageVisits,
   }
 })
