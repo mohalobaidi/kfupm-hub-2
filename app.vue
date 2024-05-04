ee09f0d366dd50d7880be128623eea4de5089230
Mohammed Alobaidi <contact@mohalobaidi.com>
2024-05-04 09:34:32 +0300

    .

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
-<template>
-  <div>
-    <NuxtWelcome />
-  </div>
+<script setup lang="ts">
+const { data: info } = await useFetch('/api/info')
+const generatedAt = useState(() => new Date().toISOString())
+</script>
+
+<template lang="pug">
+h1 Hello, World!
+p {{ info }}
+p {{ generatedAt }}
 </template>
